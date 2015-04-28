1f0279f9254eeb7dc991bc75670cb1d004c6830b
VIKRAM <VIKRAM@VIKRAM-HP.home>
2015-04-28 12:42:17 -0400

    First Commit

---
+++ FirstApp/src/firstapp/Program.java
@@ -0,0 +1,24 @@
+package firstapp;
+
+public class Program 
+{
+    public static void main(String[] args) 
+    {
+        Program obj = new Program();
+        obj.printName();
+        System.out.println("First Program : Run Successfully ");
+    }
+    public  void printName() 
+    {
+        for (int i =0;i < 3; i++)
+        {
+        System.out.println("My name is Barrey Allen");
+        }
+        
+    }
+}
+
+
+
+
+
