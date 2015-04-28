4ad0547211b71d56044ba6a21046d06719c4f84d
VIKRAM <VIKRAM@VIKRAM-HP.home>
2015-04-28 16:18:20 -0400

    Github class added

---
+++ FirstApp/src/firstapp/Github.java
@@ -0,0 +1,9 @@
+package firstapp;
+
+public class Github 
+{
+    public void gitHub() 
+    {
+        System.out.println("Github is different than git");
+    }
+}
--- FirstApp/src/firstapp/Program.java
+++ FirstApp/src/firstapp/Program.java
@@ -2,7 +2,8 @@ package firstapp;
 
 public class Program 
 {
-    Git github = new Git();
+    Git git = new Git();
+    Github github = new Github();
     public static void main(String[] args) 
     {
         Program obj = new Program();
@@ -11,10 +12,12 @@ public class Program
     }
     public  void printName() 
     {
+        git.git();
+        github.gitHub();
         for (int i =0;i < 3; i++)
         {
             System.out.println("My name is Barrey Allen");
-            github.git();
+            
         }
         
     }
